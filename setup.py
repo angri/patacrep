9b6cbea927b26e5a3fd64e78b90fe7af4fac427a
Luthaf <luthaf@yahoo.fr>
2014-05-13 21:10:37 +0100

    Ajout du support pour cx_freeze

--- setup.py
+++ setup.py
@@ -4,61 +4,26 @@
 
 $ python setup.py install
 """
-from distutils.core import setup
-from distutils.command.install import install as _install
 from songbook_core import __STR_VERSION__
-
 import sys
 import os
 import site
 
-
-def link_songbook():
-    if sys.platform.startswith('darwin'):
-        source = os.path.join(site.PREFIXES[0],
-                              'bin',
-                              'songbook')
-        dest = '/usr/local/bin/songbook'
-        if os.path.isfile(dest):
-            print("File {dest} already exist, skipping.".format(dest=dest))
-        else:
-            os.symlink(source, dest)
-    elif sys.platform.startswith('win32'):
-        script = os.path.join(site.PREFIXES[0],
-                              'Scripts',
-                              'songbook')
-        dest = script + '.py'
-        bat_name = script + '.bat'
-        if os.path.isfile(dest):
-            os.unlink(dest)
-        os.rename(script, dest)
-        content = "python {songbook} %* \n".format(songbook=dest)
-        with open(bat_name, 'w') as bat_file:
-            bat_file.write(content)
-
-
-class install(_install):
-    def run(self):
-        _install.run(self)
-        link_songbook()
-
-
-setup(cmdclass={'install': install},
-        name='songbook-core',
-        version=__STR_VERSION__,
-        description='Songbook compilation chain',
-        author='The Songbook team',
-        author_email='crep@team-on-fire.com',
-        url='https://github.com/patacrep/songbook-core',
-        packages=['songbook_core'],
-        license="GPLv2 or any later version",
-        scripts=['songbook'],
-        requires=[
+SETUP = {"name": 'songbook-core',
+        "version": __STR_VERSION__,
+        "description": 'Songbook compilation chain',
+        "author": 'The Songbook team',
+        "author_email": 'crep@team-on-fire.com',
+        "url": 'https://github.com/patacrep/songbook-core',
+        "packages": ['songbook_core'],
+        "license": "GPLv2 or any later version",
+        "scripts": ['songbook'],
+        "requires": [
             "argparse", "codecs", "distutils", "fnmatch", "glob", "json",
             "locale", "logging", "os", "plasTeX", "re", "subprocess", "sys",
             "textwrap", "unidecode", "jinja2"
             ],
-        package_data={'songbook_core': ['data/latex/*',
+        "package_data": {'songbook_core': ['data/latex/*',
                                         'data/templates/*',
                                         'data/examples/*.sb',
                                         'data/examples/*/*.sg',
@@ -67,7 +32,7 @@ setup(cmdclass={'install': install},
                                         'data/examples/*/*.png',
                                         'data/examples/*/*.png',
                                         'data/examples/*/*/header']},
-        classifiers=[
+        "classifiers": [
             "Environment :: Console",
             "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
             "Natural Language :: English",
@@ -77,5 +42,44 @@ setup(cmdclass={'install': install},
             "Programming Language :: Python :: 2.7",
             "Topic :: Utilities",
             ],
-        platforms=["GNU/Linux", "Windows", "MacOsX"],
-)
+        "platforms": ["GNU/Linux", "Windows", "MacOsX"]
+}
+
+if sys.platform.startswith('darwin'):
+    from distutils.core import setup
+    from distutils.command.install import install as _install
+    def link_songbook():
+        source = os.path.join(site.PREFIXES[0],
+                                  'bin',
+                                  'songbook')
+        dest = '/usr/local/bin/songbook'
+        if os.path.isfile(dest):
+            print("File {dest} already exist, skipping.".format(dest=dest))
+        else:
+            os.symlink(source, dest)
+
+    class install(_install):
+        def run(self):
+            _install.run(self)
+            link_songbook()
+    SETUP.update({"cmdclass": {'install': install}})
+
+elif sys.platform.startswith('win32'):
+    try:
+        from cx_Freeze import setup, Executable
+    except ImportError:
+        # Only a source installation will be possible
+        from distutils.core import setup
+    build_options = {'build_exe': {
+                        "include_files": ["plasTeX/"],
+                        "excludes": ["plasTeX"],
+                        "includes": ["UserList", "UserString", "new", "ConfigParser"]
+                    }}
+    SETUP.update({"options": build_options,
+                "executables": [Executable(script="songbook")],
+            })
+else:
+    from distutils.core import setup
+
+
+setup(**SETUP)
--- songbook_core/__init__.py
+++ songbook_core/__init__.py
@@ -2,13 +2,20 @@
 
 from pkg_resources import resource_filename
 import os
+import sys
 
 # Version
-
 __VERSION__ = (3, 7, 2)
 __STR_VERSION__ = '.'.join([str(number) for number in __VERSION__])
 
 # Directory containing shared data (default templates, custom LaTeX packages,
-# etc.)
+# etc.)	
+try:
+    __DATADIR__ = os.path.abspath(resource_filename(__name__, 'data'))
+except NotImplementedError:
+    # if current module is frozen, use library.zip path for __DATADIR__ path
+    if hasattr(sys,'frozen'):
+        __DATADIR__ = os.path.join(os.path.dirname(__file__), 'data')
+    else:
+        raise IOError("coin")
 
-__DATADIR__ = os.path.abspath(resource_filename(__name__, 'data'))
